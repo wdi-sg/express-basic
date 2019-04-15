14c11bfc4d13cec0f2cf3aab967be7f5d5b9d147
awongh <akira@awongh.com>
2019-04-15 17:42:27 +0800

    added more consoles

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 console.log("about to require express");
+
 const express = require('express');
 
 const app = express();
 console.log("done creating app");
 
 app.get('/apple', (request, response) => {
+
+  console.log("hey we got a request! yay.");
+
   response.send( "hi from aws and stuff" );
+
 })
 
 const port = parseInt( process.argv[2] );
+
 console.log("start listening");
-app.listen(port)
+
+app.listen(port, ()=> {
+  console.log("now we're listening");
+})
+
 console.log("done listening");
